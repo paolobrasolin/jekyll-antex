5d4f4a17d1e931285f8bccbdb83c59831ba9a46b
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-01-27 16:27:30 +0100

    cleanup; refactored TL setup script

---
+++ tl/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Get the dir of this script. [http://stackoverflow.com/a/246128]
+tl_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create a tmp work dir. [http://stackoverflow.com/a/34676160]
+tmp_dir=`mktemp -d -p "$tl_dir"`
+
+# Fetch and extract TeX Live installer.
+cd $tmp_dir
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-20*
+
+# Install TeX Live using custom profile.
+./install-tl --profile=$tl_dir/texlive.profile
+
+# Install extra packages.
+readarray -t texlive_packages < $tl_dir/texlive.packages
+tlmgr install ${texlive_packages[@]}
+
+# NOTE: we do no cleanup, but this is meant
+#   for a simple Travis session, so it's ok.
