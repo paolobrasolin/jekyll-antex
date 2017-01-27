8c22ccbd61a78b463ea46cf284b286086ac97734
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-01-27 17:24:35 +0100

    small tweaks to tl

--- tl/setup.sh
+++ tl/setup.sh
@@ -4,7 +4,9 @@
 tl_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create a tmp work dir. [http://stackoverflow.com/a/34676160]
-tmp_dir=`mktemp -d -p "$tl_dir"`
+# This suffices on a unix (non-mac) system:
+tmp_dir=`mktemp -d`
+# tmp_dir=`mktemp -d -p "$tl_dir"`
 
 # Fetch and extract TeX Live installer.
 cd $tmp_dir
@@ -12,14 +14,14 @@ wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz
 cd install-tl-20*
 
-# Install TeX Live using custom profile.
+# Install TeX Live using provided custom profile.
 ./install-tl --profile=$tl_dir/texlive.profile
 
 # Post install config.
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 # MEMO: "/tmp/texlive" is defined as root in "texlive.profile".
 
-# Install extra packages.
+# Install extra packages from provided list.
 readarray -t texlive_packages < $tl_dir/texlive.packages
 tlmgr install ${texlive_packages[@]}
 
