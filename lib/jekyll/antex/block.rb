c6e22e092a770c8c53d91bd16b00dfa178307024
Paolo Brasolin <paolobrasolin@Paolos-MacBook-Pro.local>
2017-09-30 03:18:53 +0200

    sync w/ antex refactor

--- lib/jekyll/antex/block.rb
+++ lib/jekyll/antex/block.rb
@@ -4,32 +4,30 @@ module Jekyll
   module Antex
     class Block < Liquid::Block
       def initialize(tag, markup, tokens)
-        super
         @markup = markup
+        super
       end
 
       def render(context)
-        options = load_options(registers: context.registers)
-        job = Antex::Compiler::Job.new(snippet: super, options: options)
+        options = build_options context: context, markup: @markup
+        job = ::Antex::Job.new snippet: super, options: options
         job.run
-        add_static_files(context.registers[:site], job)
+        add_static_file context.registers[:site], job
         job.html_tag
       end
 
       private
 
-      def add_static_files(site, job)
-        FileUtils.cp(job.file(:fit), job.file(:svg))
-        # TODO: minify/compress svg?
+      def add_static_file(site, job)
         site.static_files << Jekyll::StaticFile.new(
-          site, job.dir(:work), job.options['dest_dir'], "#{job.hash}.svg"
+          site, nil, job.options['work_dir'], "#{job.hash}.svg"
         )
       end
 
-      def load_options(markup: @markup, registers:)
+      def build_options(context:, markup:)
         Jekyll::Antex::Options.build Jekyll::Antex::Options::DEFAULTS,
-                                     registers[:site].config['antex'] || {},
-                                     registers[:page]['antex'] || {},
+                                     context.registers[:site].config['antex'] || {},
+                                     context.registers[:page]['antex'] || {},
                                      YAML.safe_load(markup) || {}
       end
     end
