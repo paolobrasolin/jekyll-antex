0a3e228a74ab4d5d71dd9f823485e2c20d8974a2
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-01 11:15:47 +0200

    add static file check to spec

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -32,5 +32,10 @@ describe Jekyll::Antex::Block do
           <p>This is my first <span class="antex"><img style="margin: 0.001ex 0.056ex -0.5ex 0.0ex; height: 2.086ex; width: 4.267ex;" src="antex/eccd48af60d2010639393a7de1b901c7.svg" /></span> paragraph.</p>
       RENDERED
     end
+
+    it 'includes rendered image into static files' do
+      expect { site.pages.first.render site.layouts, site.site_payload }
+        .to change { site.static_files.length }.by 1
+    end
   end
 end
