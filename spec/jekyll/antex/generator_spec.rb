e38cebdc8be5d0a1e5ccfd91c3cd82e3b421314b
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-07 00:20:20 +0200

    implement guarding

--- spec/jekyll/antex/generator_spec.rb
+++ spec/jekyll/antex/generator_spec.rb
@@ -87,5 +87,44 @@ describe Jekyll::Antex::Generator do
         GENERATED
       end
     end
+
+    describe 'guard/alias interaction' do
+      setup_site
+
+      setup_config <<~'CONFIG'
+        antex:
+          aliases:
+            test_alias:
+              priority: 7777777
+              regexp: 'ALI(?<code>.*?)ILA'
+          guards:
+            test_guard:
+              priority: 7777777
+              regexp: 'GUA.*?AUG'
+      CONFIG
+
+      setup_page <<~'SOURCE'
+        ---
+        GUA aaa ALI bbb ILA ccc AUG
+        aaa GUA ALI AUG bbb ILA ccc
+        aaa ALI bbb GUA ILA AUG ccc
+      SOURCE
+
+      before do
+        site.setup
+        site.read
+      end
+
+      it 'protects code from dealiasing with guards' do
+        expect { site.generate }
+          .to_not change { site.pages.first.content }
+          .from(<<~'READ')
+            ---
+            GUA aaa ALI bbb ILA ccc AUG
+            aaa GUA ALI AUG bbb ILA ccc
+            aaa ALI bbb GUA ILA AUG ccc
+        READ
+      end
+    end
   end
 end
